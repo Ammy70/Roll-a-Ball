2e3e30f10a34d002fe3f40247c74dcc8c183e07a
Aiman Ameen <160864726+Ammy70@users.noreply.github.com>
2024-11-04 14:42:49 +0500

    Create PlayerInteractionHandler.cs

---
+++ PlayerInteractionHandler.cs
@@ -0,0 +1,23 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteractionHandler : MonoBehaviour
+{
+    [SerializeField] private PlayerController _playerController;
+    // AudioSource for using to play audio in game
+   
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the object the player collided with has the "PickUp" tag.
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            // Deactivate the collided object (making it disappear).
+            other.gameObject.SetActive(false);
+            _playerController.AddCoin(1);
+        }
+    }
+}
