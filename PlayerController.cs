915e72f3586d23efff5e65d3c1f9af0bf5f6f3ad
Aiman Ameen <160864726+Ammy70@users.noreply.github.com>
2024-11-04 14:44:06 +0500

    Create PlayerController.cs

---
+++ PlayerController.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rig;
+    float moveSpeed = 10f;
+    bool useTorque = true;
+
+    private int count;
+
+    // UI text component to display count of "PickUp" objects collected.
+    public TextMeshProUGUI countText;
+
+    // UI object to display winning text.
+    public GameObject winTextObject;
+    // UI object for Audio
+    public AudioSource pop;
+    // Start is called before the first frame update.
+    void Start()
+    {
+        // Get and store the Rigidbody component attached to the player.
+        rig = GetComponent<Rigidbody>();
+
+        // Initialize count to zero.
+        count = 0;
+      
+        // Update the count display.
+        SetCountText();
+        // Initially set the win text to be inactive.
+        winTextObject.SetActive(false);
+    }
+
+    public void Jump()
+    {
+        Debug.Log("jump Input");
+    }
+    
+    public void Control(Vector3 inputVector)
+    {
+        if (useTorque)
+        {
+            rig.AddForce(inputVector * moveSpeed);
+        }
+    }
+
+    public void AddCoin(int countValue)
+    {
+        // Increment the count of "PickUp" objects collected.
+        count += countValue;
+        pop.Play();
+        SetCountText();
+    }
+
+    // Function to update the displayed count of "PickUp" objects collected.
+    void SetCountText()
+    {
+        // Update the count text with the current count.
+        countText.text = "Count: " + count.ToString();
+        // Check if the count has reached or exceeded the win condition.
+        if (count >= 3)
+        {
+            // Display the win text.
+            winTextObject.gameObject.SetActive(true);
+            // Destroy the enemy GameObject.
+            Destroy(GameObject.FindGameObjectWithTag("Enemy"));
+            
+        }
+    }
+}
